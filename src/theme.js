e44158f78b8cce4c6b65f286fcf0ec8fb2257aed
Jakub Bogdanski <bogdanskikuba@gmail.com>
2023-01-04 21:25:55 +0100

    theme color added

--- src/theme.js
+++ src/theme.js
@@ -5,5 +5,10 @@ import styled from "styled-components";
 export const theme = {
     color: {
         white: "#FFFFFF",
+        mercury: "#E5E5E5",
+        waterloo: "#7E839A",
+        mystic: "#E4E6F0",
+        pattensBlue: "#D6E4FF",
+        woodsmoke: "#18181B"
     },
 };
