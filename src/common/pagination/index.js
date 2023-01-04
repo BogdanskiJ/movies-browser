2b77aea044ba5e3bfaac3a03ec6cdd073ef8d0ff
Jakub Bogdanski <bogdanskikuba@gmail.com>
2023-01-04 21:27:19 +0100

    Paggination component added

---
+++ src/common/pagination/index.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Form } from "react-router-dom";
+import styled from "styled-components";
+import { theme } from "../../theme";
+import { PaginationBox, PaginationLeftButton, PaginationRightButton } from "./styled"
+import { ReactComponent as NextArrow } from '../../images/nextArrow.svg'
+import { ReactComponent as PrevArrow } from '../../images/prevArrow.svg'
+import { PaginationPageNumber } from "./PaginationNumberPage";
+
+
+function Pagination() {
+    const windowWidth = window.innerWidth;
+    console.log(windowWidth)
+
+
+
+    return (
+        <PaginationBox theme={theme}>
+            {(windowWidth > 753)
+                ?
+                <>
+                    <PaginationLeftButton >
+                        <PrevArrow />
+                        First
+                    </PaginationLeftButton>
+
+                    <PaginationLeftButton>
+                        <PrevArrow />
+                        Previous
+                    </PaginationLeftButton>
+
+                    <PaginationPageNumber />
+
+                    <PaginationRightButton>
+                        Next
+                        <NextArrow />
+                    </PaginationRightButton>
+
+                    <PaginationRightButton>
+                        Last
+                        <NextArrow />
+                    </PaginationRightButton>
+                </>
+                :
+                <>
+                    <PaginationLeftButton >
+                        <PrevArrow />
+                        <PrevArrow />
+                    </PaginationLeftButton>
+                    <PaginationLeftButton>
+                        <PrevArrow />
+                    </PaginationLeftButton>
+                    <PaginationPageNumber />
+                    <PaginationRightButton>
+                        <NextArrow />
+                    </PaginationRightButton>
+                    <PaginationRightButton>
+                        <NextArrow />
+                        <NextArrow />
+                    </PaginationRightButton></>}
+
+
+        </PaginationBox>
+    )
+};
+export default Pagination;
+
+
