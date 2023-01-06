a5abbaac79d2e2260db74494d1c06e06f2feaded
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-01-06 15:58:25 +0100

    camera icon created

---
+++ src/common/Navigation/IconNavigation/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Icon = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+export const Item = styled.span`
+    margin-left: -1px;
+    margin-top: 4px;
+`;
