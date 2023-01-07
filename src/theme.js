b264f652573a27d43d721b8a4a569a5c09ce25f4
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-01-07 11:55:09 +0100

    new color added

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,8 @@ export const theme = {
     color: {
         white: "#FFFFFF",
         mercury: "#E5E5E5",
+        scienceBlue: "#0044CC",
+        woodSmoke: "#18181B",
     },
     breakpoint: {
         mobileMax: 320,
