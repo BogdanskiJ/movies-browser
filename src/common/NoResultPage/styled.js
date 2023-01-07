495f35aaa4290977751fa493c8d40f5a13f11bbf
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-01-07 12:24:16 +0100

    media query added

--- src/common/NoResultPage/styled.js
+++ src/common/NoResultPage/styled.js
@@ -7,10 +7,21 @@ export const StyledHeader = styled.header`
     color: ${({theme}) => theme.color.woodSmoke};
     margin-left: 276px;
     margin-top: 56px;
+
+    @media (max-width:767px){
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 130%;
+        margin-left: 16px;
+    };
 `;
 
 export const Container = styled.div`
     display: flex;
     justify-content: center;
     margin-top: 40px;
+
+    @media (max-width:767px){
+        margin-top: -100px;
+    };
 `;
