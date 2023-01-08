acef7d04d394900f3a35d27be5e4896df182df68
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-01-08 18:40:06 +0100

    resolved conflict

--- src/theme.js
+++ src/theme.js
@@ -2,18 +2,11 @@ export const theme = {
     color: {
         white: "#FFFFFF",
         mercury: "#E5E5E5",
-<<<<<<< HEAD
-
-=======
->>>>>>> fbb9284a0d3f2a4280e05dd33bd2b59fdd91bde9
         waterloo: "#7E839A",
         mystic: "#E4E6F0",
         pattensBlue: "#D6E4FF",
         woodsmoke: "#18181B"
-<<<<<<< HEAD
-=======
     },
->>>>>>> fbb9284a0d3f2a4280e05dd33bd2b59fdd91bde9
     breakpoint: {
         mobileMax: 320,
 
