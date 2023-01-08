14b8a24d97a55a74d2d41965b1ff7b63913c6190
Jakub Bogdanski <bogdanskikuba@gmail.com>
2023-01-08 21:45:07 +0100

    remove unnecessary spaces and imported files

--- src/features/movieDetails/movieMainPoster/movieMainPosterRating/index.js
+++ src/features/movieDetails/movieMainPoster/movieMainPosterRating/index.js
@@ -1,23 +1,19 @@
 import React from "react";
-import styled from "styled-components";
-import { theme } from "../../../../theme";
 import { MaxRating, MovieDescription, MovieDetail, MovieDetailsss, MovieRating, MovieTags, MovieTitle, MovieYear, Poster, Rating, Tag, VotesNumber } from "./styled";
-import poster from "../../../../images/poster.png"
 import { ReactComponent as Star } from '../../../../images/star.svg'
 
-export const MovieMainPosterRating = ({movieTitle, movieRating, votesNumber, maxRating}) => (
-        <MovieDescription>
-            <MovieDetail>
-                <MovieTitle>
-                    {movieTitle}
-                </MovieTitle>
-                <MovieRating>
-                    <Star />
-                    <Rating>{movieRating}</Rating>
-                    <MaxRating>/ {maxRating}</MaxRating>
-
-                </MovieRating>
-                <VotesNumber>{votesNumber} votes</VotesNumber>
-            </MovieDetail>
-        </MovieDescription>
+export const MovieMainPosterRating = ({ movieTitle, movieRating, votesNumber, maxRating }) => (
+    <MovieDescription>
+        <MovieDetail>
+            <MovieTitle>
+                {movieTitle}
+            </MovieTitle>
+            <MovieRating>
+                <Star />
+                <Rating>{movieRating}</Rating>
+                <MaxRating>/ {maxRating}</MaxRating>
+            </MovieRating>
+            <VotesNumber>{votesNumber} votes</VotesNumber>
+        </MovieDetail>
+    </MovieDescription>
 );
