a87717885e4aaca95243ea7c869c74b021290ea2
Jakub Bogdanski <bogdanskikuba@gmail.com>
2023-01-09 12:56:16 +0100

    add props to MovieBrowser

--- src/features/index.js
+++ src/features/index.js
@@ -5,12 +5,15 @@ import { theme } from "../theme";
 
 export const MovieBrowser = () => (
     <Container theme={theme}>
-
-        <MovieList />
-
+        <MovieList
+            movieTitle={"Mulan"}
+            movieYear={"2022"}
+            tag1={"Action"}
+            tag2={"Drama"}
+            tag3={"Comedy"}
+            tag4={"Horror"}
+            movieRating={"7,5"}
+            votesNumber={"350"}
+        />
     </Container>
-
-
-
-
 )
