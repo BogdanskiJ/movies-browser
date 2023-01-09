017fb1e65bb2e831d6a749401cad659a94a909dd
Jakub Bogdanski <bogdanskikuba@gmail.com>
2023-01-09 12:56:37 +0100

    add props to MovieList

--- src/features/movieList/index.js
+++ src/features/movieList/index.js
@@ -1,11 +1,9 @@
-import  Pagination  from "../../common/pagination";
+import Pagination from "../../common/pagination";
 import { theme } from "../../theme";
 import { Movie } from "./Movie";
 import { MovieListPage, MoviesList, PopularMoviesBox, PopularMoviesName } from "./styled";
 
-
-
-export const MovieList = () => (
+export const MovieList = ({ movieTitle, movieYear, tag1, tag2, tag3, tag4, movieRating, votesNumber }) => (
 
   <MovieListPage theme={theme}>
     <PopularMoviesBox>
@@ -13,14 +11,80 @@ export const MovieList = () => (
         Popular Movies
       </PopularMoviesName>
       <MoviesList>
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
+        <Movie
+          movieTitle={movieTitle}
+          movieYear={movieYear}
+          tag1={tag1}
+          tag2={tag2}
+          tag3={tag3}
+          tag4={tag4}
+          movieRating={movieRating}
+          votesNumber={votesNumber}
+        />
+        <Movie
+          movieTitle={movieTitle}
+          movieYear={movieYear}
+          tag1={tag1}
+          tag2={tag2}
+          tag3={tag3}
+          tag4={tag4}
+          movieRating={movieRating}
+          votesNumber={votesNumber}
+        />
+        <Movie
+          movieTitle={movieTitle}
+          movieYear={movieYear}
+          tag1={tag1}
+          tag2={tag2}
+          tag3={tag3}
+          tag4={tag4}
+          movieRating={movieRating}
+          votesNumber={votesNumber}
+        />
+        <Movie
+          movieTitle={movieTitle}
+          movieYear={movieYear}
+          tag1={tag1}
+          tag2={tag2}
+          tag3={tag3}
+          tag4={tag4}
+          movieRating={movieRating}
+          votesNumber={votesNumber}
+        />
+        <Movie
+          movieTitle={movieTitle}
+          movieYear={movieYear}
+          tag1={tag1}
+          tag2={tag2}
+          tag3={tag3}
+          tag4={tag4}
+          movieRating={movieRating}
+          votesNumber={votesNumber}
+        />
+        <Movie
+          movieTitle={movieTitle}
+          movieYear={movieYear}
+          tag1={tag1}
+          tag2={tag2}
+          tag3={tag3}
+          tag4={tag4}
+        />
+        <Movie
+          movieTitle={movieTitle}
+          movieYear={movieYear}
+          tag1={tag1}
+          tag2={tag2}
+          tag3={tag3}
+          tag4={tag4}
+        />
+        <Movie
+          movieTitle={movieTitle}
+          movieYear={movieYear}
+          tag1={tag1}
+          tag2={tag2}
+          tag3={tag3}
+          tag4={tag4}
+        />
       </MoviesList>
       <Pagination />
     </PopularMoviesBox>
