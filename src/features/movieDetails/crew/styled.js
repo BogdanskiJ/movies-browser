c9e4469673c4dcd8df190adba50fc1d260545912
Jakub Bogdanski <bogdanskikuba@gmail.com>
2023-01-10 14:32:36 +0100

    remove unnecessary space and comments

--- src/features/movieDetails/crew/styled.js
+++ src/features/movieDetails/crew/styled.js
@@ -7,13 +7,7 @@ margin:auto;
 display: flex;
 flex-direction: column;
 gap: 32px;
-
 margin-bottom:103px;
-
-/* flex-direction: row;
-align-items: flex-start;
-padding: 40px;
- */
 `;
 
 export const Title = styled.div`
