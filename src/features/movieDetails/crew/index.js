745a5c927132de5e946aa0ab0bdb90327e45617b
Jakub Bogdanski <bogdanskikuba@gmail.com>
2023-01-10 14:30:06 +0100

    rename Title in CrewBox

--- src/features/movieDetails/crew/index.js
+++ src/features/movieDetails/crew/index.js
@@ -4,7 +4,7 @@ import { CrewBox, People, Title } from "./styled";
 export const Crew = () => (
 
     <CrewBox>
-        <Title>Cast</Title>
+        <Title>Crew</Title>
         <People>After adding the repositories, I will add a container from Kuba "People" with param "name" "workplace"</People>
     </CrewBox>
 );
