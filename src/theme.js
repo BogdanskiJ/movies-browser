f8c675180b984cd84dc1891fdfa5e91530cd1b20
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-01-11 13:15:48 +0100

    typo corrected

--- src/theme.js
+++ src/theme.js
@@ -6,10 +6,10 @@ export const theme = {
         waterloo: "#7E839A",
         mystic: "#E4E6F0",
         pattensBlue: "#D6E4FF",
-        woodsmoke: "#18181B",
+        woodSmoke: "#18181B",
         black: "#000000",
         stormGrey: "#74788B",
-        whisper: "#F5F5FA"
+        whisper: "#F5F5FA",
         scienceBlue: "#0044CC",
     },
     breakpoint: {
