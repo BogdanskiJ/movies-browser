8f6d21100d65182ef01b23160ffb85d972588c01
Jakub Bogdanski <bogdanskikuba@gmail.com>
2023-01-13 11:03:59 +0100

    add file with link to api

---
+++ src/apiSet.js
@@ -0,0 +1,7 @@
+export const keyAPI = "api_key=9515ffc857c67f1558538dad140abb29";
+
+export const languageAPI = "&language=en-US";
+
+export const movieId = "746176?";
+
+export const movieDetailApiLink = "https://api.themoviedb.org/3/movie/";
