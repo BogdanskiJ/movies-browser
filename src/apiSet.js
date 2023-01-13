ce137a78c7c06e583c2cfb6896449fee0493a42c
Jakub Bogdanski <bogdanskikuba@gmail.com>
2023-01-13 21:30:08 +0100

    add necessary link to apiSet.js

--- src/apiSet.js
+++ src/apiSet.js
@@ -3,5 +3,7 @@ export const keyAPI = "api_key=9515ffc857c67f1558538dad140abb29";
 export const languageAPI = "&language=en-US";
 
 export const movieId = "746176?";
-
 export const movieDetailApiLink = "https://api.themoviedb.org/3/movie/";
+
+export const movieListPopularApiLink= "https://api.themoviedb.org/3/movie/popular?"
+export const movieListPopularPageApiLink= "&page=1"
