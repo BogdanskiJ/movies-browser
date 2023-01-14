35e254074452e80605feaec7f89898b5219a0aed
Jakub Bogdanski <bogdanskikuba@gmail.com>
2023-01-14 19:18:38 +0100

    code for get data about movie genres

--- src/features/movieList/index.js
+++ src/features/movieList/index.js
@@ -2,17 +2,25 @@ import { useEffect, useState } from "react";
 import Pagination from "../../common/pagination";
 import { theme } from "../../theme";
 import { useDataFromAPI } from "./dataFromAPI";
+import { useGenresFromAPI } from "./genresList";
 import Movie from "./Movie";
 import { MovieListPage, MoviesList, PopularMoviesBox, PopularMoviesName } from "./styled";
 
 const MovieList = ({ }) => {
 
   const dataFromAPI = useDataFromAPI();
+  const genresFromAPI = useGenresFromAPI([]);
 
   const [moviesArray, setMoviesArray] = useState([]);
   useEffect(() => {
     setMoviesArray(dataFromAPI.data.results);
   }, [dataFromAPI]);
+
+  const [genresArray, setGenresArray] = useState([]);
+  useEffect(() => {
+    setGenresArray(genresFromAPI.genres.genres);
+  }, [genresFromAPI]);
+
 
   return (
     <MovieListPage theme={theme}>
@@ -25,6 +33,7 @@ const MovieList = ({ }) => {
           {(moviesArray !== undefined
             ?
             (moviesArray.map(movie => <Movie
+              genresArray={genresArray}
               movieTitle={movie.title}
               key={movie.id}
               movieRating={movie.vote_average}
