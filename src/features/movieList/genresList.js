97ebe9c2496e7955551678984f949394cbe113db
Jakub Bogdanski <bogdanskikuba@gmail.com>
2023-01-14 19:17:35 +0100

    get data about movie genres

---
+++ src/features/movieList/genresList.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { keyAPI, languageAPI, movieDetailApiLink, movieId, movieListPopularApiLink, movieListPopularPageApiLink } from "../.././apiSet";
+
+export const useGenresFromAPI = () => {
+
+  const [genresFromAPI, setGenresFromAPI] = useState({
+    genres:[],
+    state: "loading"
+  });
+
+  useEffect(() => {
+    const fetchResponse = async () => {
+      try {
+        const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=9515ffc857c67f1558538dad140abb29&language=en-US`);
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const genres = await response.json();
+        setGenresFromAPI({
+          genres,
+          state: "success"
+        });
+      } catch {
+        setGenresFromAPI({
+          state: "fail"
+        });
+      }
+    };
+    setTimeout(fetchResponse, 1000);
+  }, []);
+  return genresFromAPI;
+};
+
