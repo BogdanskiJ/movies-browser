ddf145c9b882ea5a7a0c546890ade4b2d3835c5c
Jakub Kedzierski <kedzierski.jakub92@gmail.com>
2023-01-20 02:11:17 +0100

    added scrolling

--- src/features/Description/Projects/styled.js
+++ src/features/Description/Projects/styled.js
@@ -7,14 +7,25 @@ margin-top: 64px;
 `
 export const TileWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
   grid-template-rows: repeat(1, 1fr);
   margin-bottom: 24px;
-`
+  max-width: 100%;
+  grid-auto-flow: column;
+  grid-auto-columns: 25%;
+  overflow-x: auto;
+  padding-bottom: 10px;
 
+`
+export const MovieScroll = styled.div`
+display: flex;
+flex-wrap: nowrap;
+
+
+`
 export const Title = styled.span`
 font-weight: 600;
 font-size: 36px;
 color: ${({ theme}) => theme.color.woodSmoke};
 margin-bottom: 24px;
+margin-right: 10px;
 `
