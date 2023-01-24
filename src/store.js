1e6827ba1035a9828ad9d4274295772cd1444814
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-01-24 13:22:04 +0100

    saga added

--- src/store.js
+++ src/store.js
@@ -1,12 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "@redux-saga/core";
 import movieListReducer from "./features/movieList/movieListSlice";
 import movieDetailsReducer from "./features/movieDetails/movieDetailsSlice";
 import peopleListReducer from "./features/people/PeoplePage/peopleListSlice";
 
-export default configureStore({
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
     reducer: {
         movieList: movieListReducer,
         movieDetails: movieDetailsReducer,
         peopleList: peopleListReducer,
     },
+    middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
