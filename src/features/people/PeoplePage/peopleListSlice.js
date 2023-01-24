615509e9e285c276a5c773a1b76e764570becfb6
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-01-24 17:07:46 +0100

    fetchPeopleList, fetchPeopleListSuccess and fetchPeopleListError added

--- src/features/people/PeoplePage/peopleListSlice.js
+++ src/features/people/PeoplePage/peopleListSlice.js
@@ -8,9 +8,25 @@ const peopleListSlice = createSlice({
         query: "",
     },
     reducers: {
-
+        fetchPeopleList: (state) => {
+            state.status = "loading";
+        },
+        fetchPeopleListSuccess: (state, { payload: people }) => {
+            state.status = "success";
+            state.people = people.results;
+        },
+        fetchPeopleListError: (state) => {
+            state.status = "error";
+        },
     },
 });
 
+export const {
+    fetchPeopleList,
+    fetchPeopleListSuccess,
+    fetchPeopleListError,
+} = peopleListSlice.actions;
 export const selectPeopleListState = state => state.peopleList;
+export const selectPeopleList = state => selectPeopleListState(state).people;
+export const selectPeopleListStatus = state => selectPeopleListState(state).status;
 export default peopleListSlice.reducer;
