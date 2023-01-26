2aa0a1a97faf9d76dc5c2bd42d60056f7e983d25
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-01-26 22:16:18 +0100

    new reducers  and new selector added

--- src/features/people/PeoplePage/peopleListSlice.js
+++ src/features/people/PeoplePage/peopleListSlice.js
@@ -1,23 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const peopleListSlice = createSlice({
-  name: "people",
-  initialState: {
-    people: [],
-  },
-  reducers: {
-    fetchPeopleList: () => {},
-    setPeopleList: (state, { payload: people }) => {
-      state.people = people;
+    name: "people",
+    initialState: {
+        people: [],
     },
-    setPeopleListSucces: (state, { payload: people }) => {
-      state.loading = false;
-      state.people = people;
+    reducers: {
+        fetchPeopleList: (state) => { 
+            state.loading = true;
+        },
+        setPeopleListSucces: (state, { payload: people }) => {
+            state.people = people;
+            state.loading = false;
+        },
+        setPeopleListError: (state) => {
+            state.loading = false;
+        },
     },
-  },
 });
 
-export const { fetchPeopleList, setPeopleList, setPeopleListSucces } =
-  peopleListSlice.actions;
+export const { fetchPeopleList, setPeopleList, setPeopleListSucces, setPeopleListError } =
+    peopleListSlice.actions;
 export const selectPeopleListState = (state) => state.peopleList;
+export const selectLoadingStatus = state => selectPeopleListState(state).loading;
 export default peopleListSlice.reducer;
