926650c367ee1da5d0f93e24ae7d61fb32c246f6
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-01-26 21:55:45 +0100

    title added as props

--- src/common/LoadingPage/index.js
+++ src/common/LoadingPage/index.js
@@ -1,9 +1,9 @@
 import loadingIcon from "./LoadingIcon/icon-spinner.svg";
 import { Container, LoadingIcon, StyledHeader } from "./styled";
 
-export const LoadingPage = () => (
+export const LoadingPage = ({title}) => (
     <>
-        <StyledHeader>Search results for "Mulan"</StyledHeader>
+        <StyledHeader>{title}</StyledHeader>
         <Container>
             <LoadingIcon src={loadingIcon} alt="" />
         </Container>
