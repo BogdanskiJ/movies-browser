4976b0a9ea224c04fc343c2736321bc6bf1a5a98
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-01-28 18:00:55 +0100

    saga name changed

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,11 +1,11 @@
 import {all} from "redux-saga/effects"
-import { watchFetchMovieList } from "./features/movieList/movieListSaga";
+import { movieListSaga } from "./features/movieList/movieListSaga";
 import { watchFetchPeopleList } from "./features/people/PeoplePage/peopleSaga"
 
 
 export default function* rootSaga() {
     yield all([
         watchFetchPeopleList(),
-        watchFetchMovieList(),
+        movieListSaga(),
     ]);
 };
