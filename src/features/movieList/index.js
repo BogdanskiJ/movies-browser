f07a6758545a853451194dc5a37f31580ce48fc2
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-01-28 20:52:26 +0100

    loading page and error page added

--- src/features/movieList/index.js
+++ src/features/movieList/index.js
@@ -3,13 +3,16 @@ import { useDispatch, useSelector } from "react-redux";
 import Pagination from "./pagination/index";
 import { theme } from "../../theme";
 import Movie from "./Movie";
-import { fetchGenresList, fetchMovieList, selectGenresList, selectMovieList, setCurrentPageAPI } from "./movieListSlice";
+import { fetchGenresList, fetchMovieList, selectGenresList, selectMovieList, selectMovieListStatus, setCurrentPageAPI } from "./movieListSlice";
 import { MovieListPage, MoviesList, PopularMoviesBox, PopularMoviesName } from "./styled";
+import { LoadingPage } from "../../common/LoadingPage";
+import { ErrorPage } from "../../common/ErrorPage";
 
 const MovieList = ({ }) => {
   const dispatch = useDispatch();
   const movies = useSelector(selectMovieList);
   const genres = useSelector(selectGenresList);
+  const status = useSelector(selectMovieListStatus);
 
   useEffect(() => {
     dispatch(fetchMovieList());
@@ -22,41 +25,34 @@ const MovieList = ({ }) => {
   }, [dispatch]);
 
   return (
-    <MovieListPage theme={theme}>
-      <PopularMoviesBox>
-        <PopularMoviesName>
-          Popular Movies
-        </PopularMoviesName>
-        <MoviesList
-        >
-          {((movies !== undefined && movies !== 0)
-            ?
-            (movies.map(movie => <Movie
-              genres={genres}
-              movieTitle={movie.title}
-              key={movie.id}
-              movieRating={movie.vote_average}
-              votesNumber={movie.vote_count}
-              movieTagArray={movie.genre_ids}
-              movieYear={movie.release_date}
-              moviePosterApiLink={movie.poster_path}
-              id={movie.id}
-            >
-            </Movie>))
-            :
-            (""))
-          }
-        </MoviesList>
-        {((movies !== undefined && movies !== 0)
-          ?
-          (<Pagination
-          // total_pages={dataFromAPI.data.total_pages}
-          />)
-          :
-          (""))
-        }
-      </PopularMoviesBox>
-    </MovieListPage>
+    <>
+      {status === "loading" ? <LoadingPage title={"Loading..."} />
+        : status === "error" ? <ErrorPage />
+          : (
+            <MovieListPage theme={theme}>
+              <PopularMoviesBox>
+                <PopularMoviesName>
+                  Popular Movies
+                </PopularMoviesName>
+                <MoviesList>
+                  {(movies.map(movie => <Movie
+                    genres={genres}
+                    movieTitle={movie.title}
+                    key={movie.id}
+                    movieRating={movie.vote_average}
+                    votesNumber={movie.vote_count}
+                    movieTagArray={movie.genre_ids}
+                    movieYear={movie.release_date}
+                    moviePosterApiLink={movie.poster_path}
+                    id={movie.id}
+                  >
+                  </Movie>))}
+                </MoviesList>
+                <Pagination />
+              </PopularMoviesBox>
+            </MovieListPage>
+          )}
+    </>
   );
-}
+};
 export default MovieList;
