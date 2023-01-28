dc37bd335e22029d9e084592bee7178dc8f95f05
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-01-28 13:30:17 +0100

    watchFetchMovieList added

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,9 +1,11 @@
 import {all} from "redux-saga/effects"
+import { watchFetchMovieList } from "./features/movieList/movieListSaga";
 import { watchFetchPeopleList } from "./features/people/PeoplePage/peopleSaga"
 
 
 export default function* rootSaga() {
     yield all([
         watchFetchPeopleList(),
-    ])
-}
+        watchFetchMovieList(),
+    ]);
+};
