2bf2bdc9c6baee42840f11344ae73778dbdd8e92
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-01-28 13:33:06 +0100

    fetchMovieList, fetchMovieListSuccess, fetchMovieListError added

--- src/features/movieList/movieListSlice.js
+++ src/features/movieList/movieListSlice.js
@@ -8,18 +8,33 @@ const movieListSlice = createSlice({
         query: "",
         currentPage: 1,
         dataFromAPI: {},
-        firstPage:1,
+        firstPage: 1,
         lastPage: 500,
-        id:1,
+        id: 1,
     },
     reducers: {
         setCurrentPage: (state, { payload: actions }) => {
             state.currentPage = state.currentPage + actions;
-           // console.log(state.currentPage)
+            // console.log(state.currentPage)
+        },
+        fetchMovieList: (state) => {
+            state.status = "loading";
+        },
+        fetchMovieListSuccess: (state, { payload: movies }) => {
+            state.status = "success";
+            state.movies = movies.results;
+        },
+        fetchMovieListError: (state) => {
+            state.status = "error";
         },
     },
 });
 
-export const { setCurrentPage } = movieListSlice.actions;
+export const { setCurrentPage,
+    fetchMovieList,
+    fetchMovieListSuccess,
+    fetchMovieListError
+} = movieListSlice.actions;
 export const selectMovieListState = state => state.movieList;
+export const selectMovieList = state => selectMovieListState(state).movies;
 export default movieListSlice.reducer;
