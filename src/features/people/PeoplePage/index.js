22a4bd004a58af139b2e083d205560bd0176106d
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-01-29 20:54:20 +0100

    added people search

--- src/features/people/PeoplePage/index.js
+++ src/features/people/PeoplePage/index.js
@@ -3,18 +3,21 @@ import Pagination from "../../../common/pagination";
 import { PeopleTile } from "../PeopleTile";
 import { Background, Info, MainWrapper, TileWrapper } from "./styled";
 import { useSelector } from "react-redux";
-import { fetchPeopleList, selectLoadingStatus, selectPeopleListState } from "./peopleListSlice";
+import { fetchPeopleList, selectLoadingStatus, selectPeopleListState, selectPeopleTotalResults } from "./peopleListSlice";
 import { useDispatch } from "react-redux";
 import { LoadingPage } from "../../../common/LoadingPage";
 import { ErrorPage } from "../../../common/ErrorPage";
+import { useQueryParameter } from "../../../queryParameters";
+import searchQueryParamName from "../../../searchQueryParamName";
 
 export const People = () => {
-
   const [currentPage, setCurrentPage] = useState(1);
 
   const { people } = useSelector(selectPeopleListState);
   const loadingStatus = useSelector(selectLoadingStatus);
   const dispatch = useDispatch();
+  const query = useQueryParameter(searchQueryParamName);
+  const totalResults = useSelector(selectPeopleTotalResults);
 
   useEffect(() => {
     dispatch(fetchPeopleList())
@@ -28,7 +31,10 @@ export const People = () => {
             <>
               <Background>
                 <MainWrapper>
-                  <Info>Popular people</Info>
+                  <Info>
+                    {!query ? "Popular people" 
+                    : `Search for "${query}" (${totalResults})`}
+                    </Info>
                   <TileWrapper>
                     {people.map((actor) => (
                       <PeopleTile name={actor.name} key={actor.id} {...actor} />
