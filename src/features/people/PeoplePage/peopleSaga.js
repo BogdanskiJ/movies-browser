35701db780e82eee38b6ccd60873aa767f0281e1
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-01-29 20:53:50 +0100

    added people search

--- src/features/people/PeoplePage/peopleSaga.js
+++ src/features/people/PeoplePage/peopleSaga.js
@@ -1,20 +1,22 @@
 import { call, delay, put, takeEvery } from "redux-saga/effects";
+import { searchPeople } from "../../../getApi";
 import { getPeopleList } from "./getPeopleList";
-import { fetchPeopleList, setPeopleList, setPeopleListError, setPeopleListSucces } from "./peopleListSlice";
+import { fetchPeopleList, selectPeopleQuery, setPeopleListError, setPeopleListSucces } from "./peopleListSlice";
 
 function* fetchPeopleListHandler() {
   try {
     yield delay(500);
-    const peopleList = yield call(getPeopleList);
+    const query = yield (selectPeopleQuery);
+    const peopleList = yield query === "" ? call(getPeopleList) : call(searchPeople, query);
     yield put(setPeopleListSucces(peopleList.results));
   } catch (error) {
     yield put(setPeopleListError());
     yield call(alert, "Something went wrong...");
-  }
-}
+  };
+};
 
 export function* watchFetchPeopleList() {
   yield takeEvery(fetchPeopleList.type, fetchPeopleListHandler);
-}
+};
 
 
