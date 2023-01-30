1b4b543766e82fd40bc41d60bae160f72d77a21a
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-01-30 15:02:00 +0100

    added new reducer and new selector

--- src/features/people/PeoplePage/peopleListSlice.js
+++ src/features/people/PeoplePage/peopleListSlice.js
@@ -12,19 +12,30 @@ const peopleListSlice = createSlice({
             state.loading = true;
         },
         setPeopleListSucces: (state, { payload: people }) => {
-            state.people = people;
+            state.people = people.results;
             state.loading = false;
+            state.totalResults = people.total_results;
         },
         setPeopleListError: (state) => {
             state.loading = false;
         },
+        getQuery: (state, {payload: query}) => {
+            state.loading = true;
+            state.query = query;
+        },
     },
 });
 
-export const { fetchPeopleList, setPeopleList, setPeopleListSucces, setPeopleListError } =
-    peopleListSlice.actions;
-export const selectPeopleListState = (state) => state.peopleList;
+export const { 
+    fetchPeopleList, 
+    setPeopleList, 
+    setPeopleListSucces, 
+    setPeopleListError ,
+    getQuery,
+} = peopleListSlice.actions;
+export const selectPeopleListState = state => state.peopleList;
 export const selectLoadingStatus = state => selectPeopleListState(state).loading;
 export const selectPeopleQuery = state => selectPeopleListState(state).query;
 export const selectPeopleTotalResults = state => selectPeopleListState(state).totalResults;
+export const selectPeopleList = state => selectPeopleListState(state).people;
 export default peopleListSlice.reducer;
