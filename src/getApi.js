dbc6142b1e2f32396a6bc96ed3ea1faf74b5a205
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-01-30 13:33:59 +0100

    parameter changed

--- src/getApi.js
+++ src/getApi.js
@@ -1,4 +1,5 @@
 import { apiKey } from "./apiKey";
+import { keyAPI } from "./apiSet";
 
 const searchMovies = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&page=1&include_adult=false`;
 
@@ -9,14 +10,17 @@ export const searchPeople = async (query) => {
     };
 
     try {
-        const response = await fetch(`https://api.themoviedb.org/3/search/person?api_key=${apiKey}=en-US&page=1`);
+        const response = await fetch(`https://api.themoviedb.org/3/search/person?${keyAPI}&language=en-US&query=${query}&page=1`);
 
         if (!response.ok) {
             throw new Error(response.statusText);
         };
 
         return await response.json();
+
     } catch (error) {
         console.log(error);
     };
 };
+
+console.log(searchPeople());
