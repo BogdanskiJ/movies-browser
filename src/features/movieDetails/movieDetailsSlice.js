1fd1afe2cd722dc51f2bf35dfd9d2f780fa48db8
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-01-30 21:24:15 +0100

    added new reducers and selectors for the saga

--- src/features/movieDetails/movieDetailsSlice.js
+++ src/features/movieDetails/movieDetailsSlice.js
@@ -6,15 +6,43 @@ const movieDetailsSlice = createSlice({
         movieDetails: [],
         movieId: null,
         status: "loading",
-
+        credits: [],
     },
     reducers: {
+        fetchMovieDetails: (state) => {
+            state.status = "loading";
+        },
+        fetchMovieDetailsSuccess: (state, { payload: movieDetails }) => {
+            state.status = "success";
+            state.movieDetails = movieDetails;
+        },
+        fetchMovieDetailsError: (state) => {
+            state.status = "error";
+        },
         setMovieId: (state, { payload: id }) => {
             state.movieId = id;
+        },
+        fetchCredits: () => {},
+        fetchCreditsSuccess: (state, {payload: credits}) => {
+            state.credits = credits;
+        },
+        fetchCreditsError: (state) => {
+            state.credits = null;
         },
     },
 });
 
-export const { setMovieId } = movieDetailsSlice.actions;
+export const {
+    fetchMovieDetails,
+    fetchMovieDetailsSuccess,
+    fetchMovieDetailsError,
+    setMovieId,
+    fetchCredits,
+    fetchCreditsSuccess,
+    fetchCreditsError,
+} = movieDetailsSlice.actions;
 export const selectMovieDetailsState = state => state.movieDetails;
+export const selectMovieDetails = state => selectMovieDetailsState(state).movieDetails;
+export const selectMovieId = state => selectMovieDetailsState(state).movieId;
+export const selectCredits = state => selectMovieDetailsState(state).credits;
 export default movieDetailsSlice.reducer;
