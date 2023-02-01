e68de93162a6bc2f91978ae4f417d021bcf60fde
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-02-01 13:38:36 +0100

    added select and removed alert

--- src/features/people/PeoplePage/peopleSaga.js
+++ src/features/people/PeoplePage/peopleSaga.js
@@ -1,17 +1,16 @@
-import { call, delay, put, takeEvery } from "redux-saga/effects";
+import { call, delay, put, select, takeEvery } from "redux-saga/effects";
 import { searchPeople } from "../../../getApi";
 import { getPeopleList } from "./getPeopleList";
 import { fetchPeopleList, selectPeopleQuery, setPeopleListError, setPeopleListSucces } from "./peopleListSlice";
 
 function* fetchPeopleListHandler() {
   try {
+    const query = yield select(selectPeopleQuery);
     yield delay(500);
-    const query = yield (selectPeopleQuery);
-    const people = yield query === "" ? call(getPeopleList) : call(searchPeople, query);
+    const people = yield !query ? call(getPeopleList) : call(searchPeople, query);
     yield put(setPeopleListSucces(people));
   } catch (error) {
     yield put(setPeopleListError());
-    yield call(alert, "Something went wrong...");
   };
 };
 
