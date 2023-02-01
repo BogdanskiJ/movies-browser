37053d0696916c59d831c87e7515abb5dab501b8
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-02-01 16:12:19 +0100

    searchMovies function added

--- src/getApi.js
+++ src/getApi.js
@@ -1,11 +1,6 @@
 import { keyAPI, languageAPI, movieDetailApiLink, movieListPopularApiLink } from "./apiSet";
 import { setCurrentPage } from "./features/movieList/movieListSlice";
 
-const searchMovies = `https://api.themoviedb.org/3/search/movie?${keyAPI}&language=en-US&page=1&include_adult=false`;
-
-const searchPeople = `https://api.themoviedb.org/3/search/person?${keyAPI}=en-US&page=1&include_adult=false`;
-
-
 export const getMovieList = async () => {
     try {
         const response = await fetch(`${movieListPopularApiLink}${keyAPI}${languageAPI}&page=${setCurrentPage}`);
@@ -54,6 +49,25 @@ export const getMovieDetails = async (id) => {
 export const getCredits = async (id) => {
     try {
         const response = await fetch(`${movieDetailApiLink}${id}/credits?${keyAPI}${languageAPI}`);
+
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        };
+
+        return await response.json();
+
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+export const searchMovies = async (query) => {
+    if (!query) {
+        return;
+    };
+
+    try {
+        const response = await fetch(`https://api.themoviedb.org/3/search/movie?${keyAPI}${languageAPI}&query=${query}&page=1`);
 
         if (!response.ok) {
             throw new Error(response.statusText);
