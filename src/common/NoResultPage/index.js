618943b4b1b894fc0f59e44862712c7468c2d356
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-02-01 15:12:27 +0100

    parameter query added

--- src/common/NoResultPage/index.js
+++ src/common/NoResultPage/index.js
@@ -1,7 +1,11 @@
+import { useQueryParameter } from "../../queryParameters";
+import searchQueryParamName from "../../searchQueryParamName";
 import { ReactComponent as NoResultIcon } from "./NoResultIcon/picture.svg";
 import { Container, StyledHeader } from "./styled";
 
-const NoResultPage = ({ query }) => {
+const NoResultPage = () => {
+    const query = useQueryParameter(searchQueryParamName);
+
     return (
         <>
             <StyledHeader>{`Sorry, there are no result for "${query}"`}</StyledHeader>
