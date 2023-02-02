5ace116dd582dad6836744ac2af0b0188abd9525
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-02-02 12:37:17 +0100

    Loading Page and Error Page added

--- src/features/movieDetails/index.js
+++ src/features/movieDetails/index.js
@@ -6,14 +6,17 @@ import { MovieDetailsPage, TileBox } from "./styled";
 import { useEffect } from "react";
 import { CastBox, Title } from "./styled";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchMovieDetails, selectCredits, selectMovieDetails, setMovieId } from "./movieDetailsSlice";
+import { fetchMovieDetails, selectCredits, selectMovieDetails, selectMovieDetailStatus, setMovieId } from "./movieDetailsSlice";
 import { useParams } from "react-router-dom";
+import { LoadingPage } from "../../common/LoadingPage";
+import { ErrorPage } from "../../common/ErrorPage";
 
 const MovieDetails = () => {
   const dispatch = useDispatch();
   const movieDetails = useSelector(selectMovieDetails);
   const credits = useSelector(selectCredits);
   const { id } = useParams();
+  const status = useSelector(selectMovieDetailStatus);
 
   useEffect(() => {
     dispatch(setMovieId(id));
@@ -21,79 +24,85 @@ const MovieDetails = () => {
   }, [id, dispatch]);
 
   return (
-    (movieDetails !== undefined)
-      ?
-      (
-        <MovieDetailsPage theme={theme}>
-          {movieDetails.backdrop_path === null ?
-            "" :
-            <MovieMainPoster
-              movieBackgroundPosterW1280={`https://image.tmdb.org/t/p/original/${movieDetails.backdrop_path}`}
-              movieTitle={movieDetails.title}
-              movieRating={parseFloat(movieDetails.vote_average).toFixed(2)}
-              votesNumber={movieDetails.vote_count}
-              maxRating={"10"}
-            />
-          }
-          <MovieTail
-            movieTilePoster={`https://image.tmdb.org/t/p/w1280/${movieDetails.poster_path}`}
-            movieTitle={movieDetails.title}
-            movieYear={movieDetails.release_date}
-            countryProductionArray={movieDetails.production_countries}
-            releaseData={movieDetails.release_date}
-            tagArray={movieDetails.genres}
-            movieRating={parseFloat(movieDetails.vote_average).toFixed(2)}
-            votesNumber={movieDetails.vote_count}
-            maxRating={"10"}
-            movieStory={movieDetails.overview}
-          />
-          <>
-            {((credits.cast !== undefined)
+    <>
+      {status === "loading" ? <LoadingPage title={"Search results for \"Movie Details\""} />
+        : status === "error" ? <ErrorPage />
+          : (
+            (movieDetails !== undefined)
               ?
-              <CastBox>
-                <Title>Cast</Title>
-                <TileBox>
-                  {credits.cast.map(cast =>
-                    <Cast
-                      key={cast.credits_id}
-                      name={cast.name}
-                      profile_path={cast.profile_path}
-                      character={cast.character}
-                      id={cast.id}
+              (
+                <MovieDetailsPage theme={theme}>
+                  {movieDetails.backdrop_path === null ?
+                    "" :
+                    <MovieMainPoster
+                      movieBackgroundPosterW1280={`https://image.tmdb.org/t/p/original/${movieDetails.backdrop_path}`}
+                      movieTitle={movieDetails.title}
+                      movieRating={parseFloat(movieDetails.vote_average).toFixed(2)}
+                      votesNumber={movieDetails.vote_count}
+                      maxRating={"10"}
                     />
-                  )}
-                </TileBox>
-              </CastBox>
+                  }
+                  <MovieTail
+                    movieTilePoster={`https://image.tmdb.org/t/p/w1280/${movieDetails.poster_path}`}
+                    movieTitle={movieDetails.title}
+                    movieYear={movieDetails.release_date}
+                    countryProductionArray={movieDetails.production_countries}
+                    releaseData={movieDetails.release_date}
+                    tagArray={movieDetails.genres}
+                    movieRating={parseFloat(movieDetails.vote_average).toFixed(2)}
+                    votesNumber={movieDetails.vote_count}
+                    maxRating={"10"}
+                    movieStory={movieDetails.overview}
+                  />
+                  <>
+                    {((credits.cast !== undefined)
+                      ?
+                      <CastBox>
+                        <Title>Cast</Title>
+                        <TileBox>
+                          {credits.cast.map(cast =>
+                            <Cast
+                              key={cast.credits_id}
+                              name={cast.name}
+                              profile_path={cast.profile_path}
+                              character={cast.character}
+                              id={cast.id}
+                            />
+                          )}
+                        </TileBox>
+                      </CastBox>
+                      :
+                      ("")
+                    )}
+                  </>
+                  <>
+                    {((credits.crew !== undefined)
+                      ?
+                      <CastBox>
+                        <Title>Crew</Title>
+                        <TileBox>
+                          {(credits.crew.map(crew =>
+                            <Cast
+                              key={crew.credit_id}
+                              name={crew.name}
+                              profile_path={crew.profile_path}
+                              character={crew.department}
+                              id={crew.id}
+                            />
+                          ))}
+                        </TileBox>
+                      </CastBox>
+                      :
+                      ("")
+                    )}
+                  </>
+                </MovieDetailsPage>
+              )
               :
               ("")
-            )}
-          </>
-          <>
-            {((credits.crew !== undefined)
-              ?
-              <CastBox>
-                <Title>Crew</Title>
-                <TileBox>
-                  {(credits.crew.map(crew =>
-                    <Cast
-                      key={crew.credit_id}
-                      name={crew.name}
-                      profile_path={crew.profile_path}
-                      character={crew.department}
-                      id={crew.id}
-                    />
-                  ))}
-                </TileBox>
-              </CastBox>
-              :
-              ("")
-            )}
-          </>
-        </MovieDetailsPage>
-      )
-      :
-      ("")
-  )
+          )}
+    </>
+  );
 };
 
 export default MovieDetails;
