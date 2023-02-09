d370f1b4abd17305a0e254caa410ff0e2cdd5e51
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2023-02-09 11:56:47 +0100

    new styled component added

--- src/common/LoadingPage/index.js
+++ src/common/LoadingPage/index.js
@@ -1,11 +1,11 @@
 import loadingIcon from "./LoadingIcon/icon-spinner.svg";
-import { Container, LoadingIcon, StyledHeader } from "./styled";
+import { Container, LoadingIcon, StyledHeader, Wrapper } from "./styled";
 
 export const LoadingPage = ({ title }) => (
-    <>
+    <Wrapper>
         <StyledHeader>{title}</StyledHeader>
         <Container>
             <LoadingIcon src={loadingIcon} alt="" />
         </Container>
-    </>
+    </Wrapper>
 );
